6e4cb8a19ebe378aa5a38c6d0c9c8140d9948b9e
scinscinscin <scinorandex1@gmail.com>
2024-05-18 17:30:29 +0800

    fix generateSaltFunction

--- src/utils/lib/generateSaltFunction.ts
+++ src/utils/lib/generateSaltFunction.ts
@@ -1,3 +1,4 @@
 import { Options, cryptoRandomStringAsync as crs } from "crypto-random-string";
 
-export const generateSaltFunction = (opts?: Options) => () => crs({ length: 128, type: "base64", ...{ opts } });
+export const generateSaltFunction = (opts?: Options) => () =>
+  crs({ length: opts?.length ?? 128, type: opts?.type ?? "base64" });
